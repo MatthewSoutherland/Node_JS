533ae0ad282e2d9aefff532e93e0c810cc6e8050
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-26 17:46:38 -0500

    feat: added moreInfo file

---
+++ 6-moreInfo.js
@@ -0,0 +1,8 @@
+module.exports.items = ["item1", "item2"];
+const person = {
+  name: "bob",
+  job: "homeless",
+};
+
+// setting up a property on export of person object
+module.exports.singlePerson = person;
