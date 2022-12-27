14d8344b16b2b95f46ef91759b8ebce2b0dd0c0f
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 12:23:45 -0500

    feat: added about page and 404

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,9 +2,23 @@ const http = require("http");
 
 // server takes a call back which is invoked everytime the user makes a request
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write(`<h1>home page</h1>`);
-  res.end();
+  const url = req.url;
+
+  if (url == "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>home page</h1>`);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1> About page </h1>`);
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(`<h1>Shit</h1>`);
+    res.end();
+  }
 });
 
 server.listen(5000);
