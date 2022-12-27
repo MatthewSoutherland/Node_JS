b27a0742e60d408084728124ffd09519ecc7d696
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-26 22:24:47 -0500

    feat: added stream file

---
+++ 16-stream.js
@@ -0,0 +1,13 @@
+const { createReadStream } = require("fs");
+
+const stream = createReadStream("./big.txt");
+
+// default 64kb
+// last buffer -remainder
+// highWaterMark - control size
+// const stream = createReadStream('./big.txt', {highWaterMark: 9000 })
+// const stream = createReadStream('./big.txt', {encoding" 'utf8' })
+
+stream.on("data", result => {
+  console.log(result);
+});
