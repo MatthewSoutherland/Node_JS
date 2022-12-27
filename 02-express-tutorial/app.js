a73f08694abfa964b15d827d075ff7b3a26fce6c
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 14:04:44 -0500

    feat: set path variable

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  console.log("user hit that shit");
-  res.send("Home Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  console.log("user hit about");
+  res.status(200).send("about page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send(`<h1> resource not found </h1>`);
 });
 
 app.listen(5000, () => {
