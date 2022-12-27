d4c5b9ad22f78d6087b502e90c5a85ba602ccc73
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 00:34:11 -0500

    feat: wrote header for server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,9 @@ const http = require("http");
 
 // server takes a call back which is invoked everytime the user makes a request
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("home page");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write(`<h1>home page</h1>`);
+  res.end();
 });
 
 server.listen(5000);
