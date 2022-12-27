5bcf9b49d0eac725ad67929855c0d1f7dae18758
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-12-27 00:31:15 -0500

    feat: created server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+// server takes a call back which is invoked everytime the user makes a request
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+  res.end("home page");
+});
+
+server.listen(5000);
